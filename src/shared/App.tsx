e717258cd708189a5dafb8c9fbca68faca76a9fb
Jungeun <kjeunn@gmail.com>
2020-01-30 00:43:26 +0900

    Fix about route Fix index and App configuration Delete client-Root.tsx

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { PaymentList, Item, Discount } from "../pages";
 
 function App() {
   return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={PaymentList} />
-        <Route exact path="/item" component={Item} />
-        <Route exact path="/discount" component={Discount} />
-      </Switch>
+    <div style={{ height: "100vh" }}>
+      <Route exact path="/" component={PaymentList} />
+      <Route exact path="/cart" component={PaymentList} />
+      <Route exact path="/item" component={Item} />
+      <Route exact path="/discount" component={Discount} />
     </div>
   );
 }
